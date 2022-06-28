a3ac2571d2d318a38d1d3d00b62487940ba75139
Herimino Rakotondrafara <heriminorakotondrafara@Heriminos-MacBook-Pro.local>
2022-06-28 19:56:57 -0400

    finalized favorites using redux

--- screens/FavoriteScreen.tsx
+++ screens/FavoriteScreen.tsx
@@ -5,13 +5,18 @@ import { FavoritesContext } from "../store/context/favorites-context";
 import { MEALS } from "../data/dummy-data";
 import Meal from "../models/meal";
 import MealsDisplayer from "../components/MealsList/MealsDisplayer";
-
+import { useSelector } from "react-redux";
 
 const FavortieScreens=({navigation}:any):any=>{
     //navigation prop is exposed to drawer navigations as well
-    const favoriteMealsContext=useContext(FavoritesContext);
-    const favoriteMeals:string[]=favoriteMealsContext.ids;//contains the array of ids
-    const favMeals:Meal[]=MEALS.filter((meal:Meal)=>favoriteMeals.includes(meal.id))
+    //const favoriteMealsContext=useContext(FavoritesContext);
+
+    const favoriteMealIds=useSelector((state:any)=>state.favoriteMeals.ids);
+
+    //const favoriteMeals:string[]=favoriteMealsContext.ids;//contains the array of ids
+
+
+    const favMeals:Meal[]=MEALS.filter((meal:Meal)=>favoriteMealIds.includes(meal.id))
     
     return (<MealsDisplayer displayedmeals={favMeals} navigation={navigation}/>)
 };
